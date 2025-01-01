316d54fc1d8ecbe462649c2da4ed24252d5420c4
Mateus Oliveira <mateuskywalker90@gmail.com>
2025-01-01 14:41:27 -0400

    created rabbitmq connection

--- src/main/java/com/mateus/proposta_app/config/RabbitMQConfiguration.java
+++ src/main/java/com/mateus/proposta_app/config/RabbitMQConfiguration.java
@@ -2,6 +2,10 @@ package com.mateus.proposta_app.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,5 +30,15 @@ public class RabbitMQConfiguration {
     @Bean
     public Queue createQueueConcludedNotification() {
         return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initializeAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
     }
 }
